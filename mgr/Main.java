1b6afef12cce8f280940798a79973f93def4ea24
monika-zielinska <monika.zielinska@zhr.pl>
2017-08-28 01:30:21 +0200

    dodane czytanie inputu - przyklad

--- mgr/Main.java
+++ mgr/Main.java
@@ -1,9 +1,14 @@
 package mgr;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Jak masz na imie?...");
+		String name = in.next();
+		System.out.println("Hello " + name + "!");
 	}
 	
 }

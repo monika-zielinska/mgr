bc7e918613bedecc71be9b2d94c7414bdf66d614
monika-zielinska <monika.zielinska@zhr.pl>
2017-09-06 15:43:22 +0200

    W jednej linijce się nie powtarzają, a linijki z liczbami przeciwnymi kasują się

--- mgr/Main.java
+++ mgr/Main.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    private int n;
+    private int n, t;
     private int[][] tab;
     private int[] wybrane;
+    private boolean[] powt = new boolean[2000];
 
-    public void loadData() {
+    //init
+    public void initData() {
+        t = 0;
+        wybrane = new int[n];
+        for (int i = 0; i < 2000; i++) {
+            powt[i] = false;
+        }//for
+    }//public void initData
+
+    public void loadData() { 
         //liczba linijek
         Scanner in = new Scanner(System.in);
         System.out.println("Podaj liczbę linijek");
@@ -17,7 +27,6 @@ public class Main {
 
         //tablica
         tab = new int[n][2];
-        wybrane = new int[n];
 
         //pętla
         for (int i = 0; i < n; i++) {
@@ -40,16 +49,38 @@ public class Main {
     public void wybierzLiczby(int i) { // wybierz liczbe z linijki i
         if (i < n) {
             // wybierz pierwsza z i-tej linijki (tab[i][0])
-            wybrane[i] = tab[i][0];
-            wybierzLiczby(i + 1);
+            int a = tab[i][0];
+            // sprawdzamy czy a bylo juz wybrane
+            if (powt[a + 1000]) {
+                wybierzLiczby(i + 1);
+            } else if (!powt[-a + 1000]) {
+                wybrane[t] = a; 
+                t++;
+                powt[a + 1000] = true;    
+                wybierzLiczby(i + 1);
+                powt[a + 1000] = false;
+                t--;
+            }//else if
+            
             // wybierz druga z i-tej linijki (tab[i][1])
-            wybrane[i] = tab[i][1];
-            wybierzLiczby(i + 1);
+            int b = tab[i][1];
+
+            if (powt[b + 1000]) {
+                wybierzLiczby(i + 1);
+            } else if (!powt[-b + 1000]) {
+                wybrane[t] = b; 
+                t++;
+                powt[b + 1000] = true;    
+                wybierzLiczby(i + 1);
+                powt[b + 1000] = false;
+                t--;
+            }//else if
+
         } else {
-            for (i = 0; i < n; i++) {
-                System.out.print(String.format("%3d", wybrane[i]) + " ");
+            for (int j = 0; j < t; j++) {
+                System.out.print(String.format("%3d", wybrane[j]) + " ");
             }//for
-            System.out.println(0);
+            System.out.println("  0");
         }//else
     }//public void wybierzLiczby
 
@@ -57,9 +88,10 @@ public class Main {
     public static void main(String[] args) {
         Main program = new Main();
         program.loadData();
+        program.initData();
         program.printData();
         System.out.println("Wybrane liczby:");
         program.wybierzLiczby(0);
-
     }//public static void main
-}//public class Main	
+}//public class Main    
+

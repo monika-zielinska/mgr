e65503ab0234c332e8afb9d43dd5f8c01c59b077
monika-zielinska <monika.zielinska@zhr.pl>
2017-09-12 15:10:13 +0200

    Program wczytuje dane na dwa sposoby: ręcznie albo losowo. Sposób definiujemy odpowiednio uruchamiając program w konsoli.

--- mgr/Main.java
+++ mgr/Main.java
@@ -1,5 +1,7 @@
 package mgr;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -18,7 +20,7 @@ public class Main {
         }//for
     }//public void initData
 
-    public void loadData() { 
+    public void loadData() {
         //liczba linijek
         Scanner in = new Scanner(System.in);
         System.out.println("Podaj liczbę linijek");
@@ -39,6 +41,34 @@ public class Main {
         } //for
     }//public void load
 
+    public void randomData(String[] args) {
+        int s;
+        if (args.length == 3) {
+            // uzyj liczb - argumentow programu
+            s = Integer.parseInt(args[1]);
+            n = Integer.parseInt(args[2]);
+        } else {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Podaj liczbę stanów");
+            s = in.nextInt();
+            System.out.println("Podaj liczbę tranzycji:");
+            n = in.nextInt();
+        }
+
+        //tablica
+        tab = new int[n][2];
+
+        //pętla
+        Random r = new Random();
+        for (int i = 0; i < n; i++) {
+            int a = r.nextInt(s) + 1;
+            int b = r.nextInt(s) + 1;
+
+            tab[i][0] = a;
+            tab[i][1] = -b;
+        } //for
+    }//public void random
+
     public void printData() {
         //wypisywanie tablicy na ekran
         for (int i = 0; i < n; i++) { // przejdz po wierszach
@@ -54,23 +84,23 @@ public class Main {
             if (powt[a + 1000]) {
                 wybierzLiczby(i + 1);
             } else if (!powt[-a + 1000]) {
-                wybrane[t] = a; 
+                wybrane[t] = a;
                 t++;
-                powt[a + 1000] = true;    
+                powt[a + 1000] = true;
                 wybierzLiczby(i + 1);
                 powt[a + 1000] = false;
                 t--;
             }//else if
-            
+
             // wybierz druga z i-tej linijki (tab[i][1])
             int b = tab[i][1];
 
             if (powt[b + 1000]) {
                 wybierzLiczby(i + 1);
             } else if (!powt[-b + 1000]) {
-                wybrane[t] = b; 
+                wybrane[t] = b;
                 t++;
-                powt[b + 1000] = true;    
+                powt[b + 1000] = true;
                 wybierzLiczby(i + 1);
                 powt[b + 1000] = false;
                 t--;
@@ -87,7 +117,20 @@ public class Main {
     // funkcja main - START programu
     public static void main(String[] args) {
         Main program = new Main();
-        program.loadData();
+
+        if (args.length == 0) {
+            program.loadData();
+            //obsluga argumentow programu
+        } else if (args[0].equals("-rand")) {
+            program.randomData(args);
+        } else { // zly argument
+            System.out.println("wybrano zla opcje!");
+            System.out.println("uzycie programu:");
+            System.out.println("\tjava -jar mgr.jar");
+            System.out.println("\tjava -jar mgr.jar -rand");
+            return;
+        }
+
         program.initData();
         program.printData();
         System.out.println("Wybrane liczby:");
